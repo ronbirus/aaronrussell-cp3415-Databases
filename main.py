56543ea47fa3e6357ccd84b747a49949510a3cac
Aaron <aaronnoraator@gmail.com>
2022-02-01 13:53:46 -0330

    added knock knock joke code

---
+++ main.py
@@ -0,0 +1,14 @@
+print ('knock, knock')
+answer1 = input('type your response:')
+while True:
+    if answer1 == "who's there?":
+        print ('dees')
+        break
+    else: print ('wrong answer!')
+answer2 = input('type your response:')
+while True:
+    if answer2 == "dees who?":
+        break
+    else: print ('wrong answer!')
+print ('dees nuts. lol goteem')
+
